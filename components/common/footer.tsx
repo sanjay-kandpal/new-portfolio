b71d0944cf0a4992388481082546c23fde9b250f
sanjay <72781686+sanjay-kandpal@users.noreply.github.com>
2025-07-26 23:32:49 +0530

    added logo in banner,removed logo unecessary

--- components/common/footer.tsx
+++ components/common/footer.tsx
@@ -48,7 +48,7 @@ const Footer = () => {
         ></Button>
       </div>
       <h2 className="text-center text-sm sm:text-base mt-8">
-        Designed and Developed with ❤️ by Ayush
+        Designed and Developed with ❤️ by Sanjay Kandpal
       </h2>
     </>
   );
